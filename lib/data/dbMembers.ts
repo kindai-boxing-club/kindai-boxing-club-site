ad6386db3be2723f41340237f7197560c2e2d907
szknuts <xeno4absolute@gmail.com>
2025-12-17 10:09:56 +0900

    feat: 部員の取得順序を学年降順に加えて役職順でソートするように変更し、関連するコメントを整理

--- lib/data/dbMembers.ts
+++ lib/data/dbMembers.ts
@@ -5,7 +5,21 @@ import { Person } from "@/types";
 export async function getMembersFromD1(db: D1Database): Promise<Person[]> {
   try {
     const { results } = await db
-      .prepare("SELECT * FROM members ORDER BY grade DESC, id ASC")
+      .prepare(
+        `
+        SELECT * FROM members 
+        ORDER BY 
+          grade DESC,
+          CASE 
+            WHEN position = '主将' THEN 1
+            WHEN position = '副主将' THEN 2
+            WHEN position = '主務' THEN 3
+            WHEN position = '会計' THEN 4
+            ELSE 5 
+          END ASC,
+          id ASC
+      `
+      )
       .all<Person>();
     return results;
   } catch (e) {
