87eda78c7b689015d7eead4947b03be0af9988a2
Keishin Suzuki <xeno4absolute@gmail.com>
2026-09-20 19:00:29 +0900

    ref: memberの型定義のfacultyを変更。nullを代入できないようにした。

--- types/index.ts
+++ types/index.ts
@@ -45,7 +45,7 @@ export interface Member extends Person {
   position: MemberPosition;
 
   is_manager: 0 | 1;
-  faculty: Faculty | null;
+  faculty: Faculty;
   weight_class: WeightClass | null;
   has_experience: 0 | 1;
 }
